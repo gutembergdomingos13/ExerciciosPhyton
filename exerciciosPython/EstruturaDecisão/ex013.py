495d7bbef9f0abfc9e57599d6fd69fbdc20df32f
Guto <gutembergdomingos13@gmail.com>
2021-10-30 00:34:14 -0300

    teste dicionário

--- exerciciosPython/EstruturaDecisão/ex013.py
+++ exerciciosPython/EstruturaDecisão/ex013.py
@@ -1,5 +1,5 @@
 # 13 Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2- Segunda, etc.),
 # se digitar outro valor deve aparecer valor inválido.
 
-diasSemana = {'domingo':1, 'segunda':2, 'terça':3, 'quarta':4, 'quinta':5, 'sexta':6, 'sábado':7 }
+diasSemana = {1:'domingo', 2:'segunda', 3:'terça', 4:'quarta', 5:'quinta', 6:'sexta', 7:'sábado'}
 
