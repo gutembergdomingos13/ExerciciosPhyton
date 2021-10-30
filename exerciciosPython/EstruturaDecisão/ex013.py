ce1ee7c7874f62bad4e0defa3058d10737d62b9f
Guto <gutembergdomingos13@gmail.com>
2021-10-30 00:24:11 -0300

    inicio de resolução questão 13 estrutura de desição

--- exerciciosPython/EstruturaDecisão/ex013.py
+++ exerciciosPython/EstruturaDecisão/ex013.py
@@ -1,3 +1,5 @@
-teste = 'casa'
+# 13 Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2- Segunda, etc.),
+# se digitar outro valor deve aparecer valor inválido.
 
-print(teste)
+diasSemana = {'domingo':1, 'segunda':2, 'terça':3, 'quarta':4, 'quinta':5, 'sexta':6, 'sábado':7 }
+
